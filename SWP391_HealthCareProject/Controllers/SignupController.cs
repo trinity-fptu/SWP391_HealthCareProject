cc06c8e885c5faa1d8e7768582cdb6e222d30aec
hainamzsd <machaidangms@gmail.com>
2022-10-20 22:05:46 +0700

    add signup

---
+++ SWP391_HealthCareProject/Controllers/SignupController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SWP391_HealthCareProject.Controllers
+{
+    public class SignupController : Controller
+    {
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
