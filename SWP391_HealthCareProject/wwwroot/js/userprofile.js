5a418bb85cf219f297a825f0d284f2905d5fc069
hainamzsd <machaidangms@gmail.com>
2022-10-23 20:46:15 +0700

    small fix

--- SWP391_HealthCareProject/wwwroot/js/userprofile.js
+++ SWP391_HealthCareProject/wwwroot/js/userprofile.js
@@ -1,21 +1,27 @@
 ﻿
+
 function openTab(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
+    tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
+        tablinks[i].style.backgroundColor = "#F9F9F8";
     }
 
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
+    
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].style.backgroundColor = "#F9F9F8";
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    evt.currentTarget.style.backgroundColor = "#FF6262";
+
+    //doi mau = js
 }
