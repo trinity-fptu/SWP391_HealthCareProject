6c6775ea5e1520b02859300e53ccce65ae52041d
bachnguyen273 <nguyenvuongbach933@gmail.com>
2022-10-23 13:15:10 +0700

    Add profile ui

---
+++ SWP391_HealthCareProject/Controllers/DetailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SWP391_HealthCareProject.Controllers
+{
+    public class DetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
