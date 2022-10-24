c788b88e667754b74c9093319b0f8b822a0d5cf1
trinity-fptu <pain1601@gmail.com>
2022-10-24 19:38:15 +0700

    AdminDAO

---
+++ SWP391_HealthCareProject/DataAccess/AdminDAO.cs
@@ -0,0 +1,61 @@
+﻿using SWP391_HealthCareProject.Models;
+
+namespace SWP391_HealthCareProject.DataAccess
+{
+    public class AdminDAO
+    {
+        List<User> users = new List<User>();
+
+        //CRUD function
+        public List<User> getAllUser()
+        {
+            using var db = new BloodDonorContext();
+            users = db.Users.ToList();
+            return users;
+        }
+
+        public User getUserById(int id)
+        {
+            using var db = new BloodDonorContext();
+            var user = db.Users.Find(id);
+            return user;
+        }
+
+        public bool addUser(User user)
+        {
+            using var db = new BloodDonorContext();
+            if (db.Users.Find(user.UserName) != null)
+            {
+                return false;
+            }
+            db.Users.Add(user);
+            db.SaveChanges();
+            return true;
+        }
+
+        public bool updateUser(User user)
+        {
+            using var db = new BloodDonorContext();
+            if (db.Users.Find(user.UserName) == null)
+            {
+                return false;
+            }
+            db.Users.Update(user);
+            db.SaveChanges();
+            return true;
+        }
+
+        public bool deleteUser(User user)
+        {
+            using var db = new BloodDonorContext();
+            if (db.Users.Find(user.UserName) == null)
+            {
+                return false;
+            }
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return true;
+        }
+        
+    }
+}
