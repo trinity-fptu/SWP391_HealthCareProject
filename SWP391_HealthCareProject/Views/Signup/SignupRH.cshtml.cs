cdbaed8fcd2bff3004976d86494caba9b1c59e58
hainamzsd <machaidangms@gmail.com>
2022-10-30 21:04:20 +0700

    add register admin

---
+++ SWP391_HealthCareProject/Views/Signup/SignupRH.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SWP391_HealthCareProject.Controllers
+{
+    public class SignupRHModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
