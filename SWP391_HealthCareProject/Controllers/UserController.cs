096aa320ca07f387ad4bcaea1c0d8f385cd17177
hainamzsd <machaidangms@gmail.com>
2022-11-12 14:24:58 +0700

    userprofile update

--- SWP391_HealthCareProject/Controllers/UserController.cs
+++ SWP391_HealthCareProject/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SWP391_HealthCareProject.DataAccess;
 using SWP391_HealthCareProject.Filters;
 using SWP391_HealthCareProject.Models;
 
@@ -7,22 +8,71 @@ namespace SWP391_HealthCareProject.Controllers
     [RequestAuthentication]
     public class UserController : Controller
     {
-        public IActionResult UserProfile()
+
+        public User? GetUserSession()
         {
             if (HttpContext.Session.GetObjectFromJson<User>("User") != null)
             {
                 var userInfo = HttpContext.Session.GetObjectFromJson<User>("User");
-                if (HttpContext.Session.GetObjectFromJson<Volunteer>("Volunteer") != null)
-                {
-                    var volunteerInfo = HttpContext.Session.GetObjectFromJson<Volunteer>("Volunteer");
-                    string fullName = volunteerInfo.LastName + " " + volunteerInfo.FirstName;
-                    ViewBag.FullName = fullName;
-                }
-                ViewBag.UserName = userInfo.UserName;
-                ViewBag.Email = userInfo.Email;
+                return userInfo;
             }
-            return View();
+            return null;
         }
+
+        public IActionResult UserProfile()
+        {
+            List<User> userModel = new List<User>();
+            userModel = VolunteerDAO.getAllUser();
+            List<Volunteer> volunteerList = new List<Volunteer>();
+            VolunteerDAO vDao = new VolunteerDAO();
+            volunteerList = vDao.getAllVolunteer();
+            UserProfileModels us = new UserProfileModels();
+            us.UserViewModel = userModel;
+            us.VolunteerViewModel = volunteerList;
+
+            if (HttpContext.Session.GetObjectFromJson<User>("User") != null)
+            {
+                var userInfo = HttpContext.Session.GetObjectFromJson<User>("User");
+                var volunteer = VolunteerDAO.GetVolunteerByUserId(userInfo.UserId);
+                string fullName = volunteer.LastName + " " + volunteer.FirstName;
+                ViewBag.FullName = fullName;
+                //ViewBag.UserName = userInfo.UserName;
+                //ViewBag.Email = userInfo.Email;
+                //ViewBag.FirstName = volunteer.FirstName;
+                //ViewBag.LastName = volunteer.LastName;
+                //ViewBag.DOB = volunteer.Dob.ToString("yyyy-MM-dd");
+                //ViewBag.Gender = volunteer.Gender;
+                //ViewBag.BloodGroup = volunteer.BloodType;
+                //ViewBag.Attend = volunteer.Attended;
+                //ViewBag.IdCardNumber = volunteer.IdCardNumber;
+                //ViewBag.Phone = volunteer.Phone;
+                //ViewBag.Email = userInfo.Email;
+                //ViewBag.Address = volunteer.Address;
+                //ViewBag.City = volunteer.City;
+                //ViewBag.Avatar = userInfo.Avatar;
+                ViewBag.UserId = userInfo.UserId;
+                ViewBag.VolunteerId = volunteer.VolunteerId;
+            }
+            return View(us);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Volunteer volunteer)
+        {
+            volunteer.VolunteerId = VolunteerDAO.GetVolunteerByUserId(GetUserSession().UserId).VolunteerId;
+            volunteer.UserId = GetUserSession().UserId;
+            if (volunteer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                VolunteerDAO volunteerdao = new VolunteerDAO();
+                volunteerdao.updateVolunteer(volunteer);
+            }
+            return RedirectToAction("UserProfile", "User");
+        }
+
 
         public IActionResult RHProfile()
         {
---
+++ SWP391_HealthCareProject/DataAccess/UserProfileModels.cs
@@ -0,0 +1,19 @@
+﻿using SWP391_HealthCareProject.Models;
+
+namespace SWP391_HealthCareProject.DataAccess
+{
+    public class UserProfileModels
+    {
+        BloodDonorContext bloodDonorContext = new BloodDonorContext();
+        public List<User> UserViewModel { get; set; }
+
+        public List<Volunteer> VolunteerViewModel { get; set; }
+
+        public User GetUserById(int id) => bloodDonorContext.Users.Where(x => x.UserId == id).FirstOrDefault();
+
+        public Volunteer GetVolunteerById(int id)
+        {
+            return bloodDonorContext.Volunteers.Where(x => x.VolunteerId == id).FirstOrDefault();
+        }
+    }
+}
