96f932c849f037c732cbe16827b03a8d14f9f96d
hainamzsd <machaidangms@gmail.com>
2022-11-14 02:09:00 +0700

    nunit testing

---
+++ TestingLibrary/Class1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit;
+using NUnit.Framework;
+using SWP391_HealthCareProject.DataAccess;
+using SWP391_HealthCareProject.Models;
+using System.Data;
+
+namespace TestingLibrary
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void testUser()
+        {
+            var volunteer = VolunteerDAO.GetVolunteerByUserId(1);
+            var result = "Bach";
+            Assert.AreEqual(volunteer.LastName, result);
+        }
+
+    }
+}
